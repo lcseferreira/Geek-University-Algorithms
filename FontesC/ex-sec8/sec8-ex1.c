1b9f64e7350df631a37552e133cb2e7c684894be
Lucas Ferreira <lcseferreira@hotmail.com>
2021-11-09 14:04:58 -0300

    Ex sec8 C

---
+++ FontesC/ex-sec8/sec8-ex1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main () {
+	int vetor[5], pares[5], cont_pares = 0;
+
+	for (int i = 0; i < 5; i++) {
+		printf("Digite um número: ");
+		fflush(stdout);
+		scanf("%d", &vetor[i]);
+
+		if ((vetor[i] > 0) && (vetor[i] % 2 == 0)) {
+			pares[cont_pares] = vetor[i];
+			cont_pares += 1;
+		}
+	}
+
+	for (int i = 0; i < cont_pares; i++){
+		printf("%d\n", pares[i]);
+	}
+}
